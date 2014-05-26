460e6181e86a6075e2557708da5bf661fac6796d
matthewhampton <matthew.j.hampton@gmail.com>
2014-05-26 14:54:17 +0200

    Adding DynamicFileBasedBpmnParser that loads subworkflows dynamically (with no need to create a packager).

---
+++ tests/SpiffWorkflow/bpmn/DynamicLoadingOfSubWorkflowsTest.py
@@ -0,0 +1,37 @@
+import unittest
+import datetime
+import time
+from SpiffWorkflow.Task import Task
+from SpiffWorkflow.bpmn.BpmnWorkflow import BpmnWorkflow
+from tests.SpiffWorkflow.bpmn.BpmnWorkflowTestCase import DynamicallyLoadedSubWorkflowTestCase
+
+__author__ = 'neilc'
+
+
+class NestedProcessesTest(DynamicallyLoadedSubWorkflowTestCase):
+    def setUp(self):
+        self.spec = self.load_spec()
+
+    def load_spec(self):
+        return self.load_workflow_spec('Dynamic-Loading-Workflows/level-01.bpmn', 'level-01')
+
+    def testRunThroughHappy(self):
+
+        self.workflow = BpmnWorkflow(self.spec)
+        self.do_next_named_step('User Task 02-A')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.do_next_named_step('User Task 03-A')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+        self.assertEquals(1, len(self.workflow.get_tasks(Task.READY)))
+        self.do_next_named_step('User Task 02-B')
+        self.workflow.do_engine_steps()
+        self.save_restore()
+        self.assertEquals(0, len(self.workflow.get_tasks(Task.READY | Task.WAITING)))
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(NestedProcessesTest)
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = 2).run(suite())
